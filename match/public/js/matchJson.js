1f1db17e4e5c564ab89511f49cb29d752923c109
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-17 22:58:55 -0400

    removed single quotes

--- match/public/js/matchJson.js
+++ match/public/js/matchJson.js
@@ -1,11 +1,11 @@
 var MATCH = [
-  '{"text": "First state to legalize gay marriage", "value": "Massachusetts" }',
-  '{"text": "The last state to join the union", "value": "Hawaii" }',
-  '{"text": "Harry Truman\'s home state", "value": "Missouri" }',
-  '{"text": "Largest state by area", "value": "Alaska" }',
-  '{"text": "John Glenn\'s home state", "value": "Ohio" }',
-  '{"text": "State where Detroit Pistons first played", "value": "Indiana" }',
-  '{"text": "The first state to join the union", "value": "Delaware" }'
+  {"text": "First state to legalize gay marriage", "value": "Massachusetts" },
+  {"text": "The last state to join the union", "value": "Hawaii" },
+  {"text": "Harry Truman\'s home state", "value": "Missouri" },
+  {"text": "Largest state by area", "value": "Alaska" },
+  {"text": "John Glenn\'s home state", "value": "Ohio" },
+  {"text": "State where Detroit Pistons first played", "value": "Indiana" },
+  {"text": "The first state to join the union", "value": "Delaware" }
 ];
 
 if (typeof module != "undefined" && module.exports)
