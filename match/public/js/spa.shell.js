4069583967a750ba16b1962388e29ef1b2d917ec
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-17 21:43:04 -0400

    Added navigation bar, fixed at the top

--- match/public/js/spa.shell.js
+++ match/public/js/spa.shell.js
@@ -15,7 +15,21 @@
   var
     configMap = {
       main_html : String()
-	+ '<p>Welcome to SPA</p>'
+	+ '<nav class="navbar navbar-inverse navbar-fixed-top">'
+	  + '<div class="container-fluid">'
+	    + '<div class="navbar-header">'
+	      + '<a class="navbar-brand" href="#">ExameApp</a>'
+	    + '</div>'
+	    + '<div>'
+	      + '<ul class="nav navbar-nav">'
+	        + '<li class="active"><a href="#">Home</a></li>'
+	        + '<li><a href="#">Page 1</a></li>'
+	        + '<li><a href="#">Page 2</a></li>'
+	        + '<li><a href="#">Page 3</a></li>'
+	      + '</ul>'
+	    + '</div>'
+	  + '</div>'
+	+ '</nav>'
     },
     stateMap = { $container : null },
     jqueryMap = {},
