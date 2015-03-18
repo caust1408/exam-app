1e86e94daa46489245143d51bdf48b39d2cd13e3
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-17 22:57:35 -0400

    getting data from the JSON file

--- match/public/js/match.js
+++ match/public/js/match.js
@@ -4,8 +4,14 @@
  * contains the logic for page 1, that will represent the match problems
  */
 
-match_form = function() {
+//match_form = function() {
   
+  // get the JSON file
+  var data = require("./matchJson.js");
+
+  //var elements = JSON.parse(data);
+
+  console.log(data[1].text);
 
 
-}
+//}
