03ac84a4d13785fa97459cd0b94f85112369096d
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-03-23 22:31:11 -0400

    Creats the paragraphs and adss them to main

--- match/public/js/match.js
+++ match/public/js/match.js
@@ -4,14 +4,22 @@
  * contains the logic for page 1, that will represent the match problems
  */
 
-//match_form = function() {
-  
+match_form = function( jqueryMap ) {
+  console.log("match_form");
+
+  //jqueryMap.$main.prepend("<p>Appeneded paragraph</p>");
   // get the JSON file
-  var data = require("./matchJson.js");
-
+  //var data = require("./matchJson.js");
+  var data = MATCH; // set data to JSON data
   //var elements = JSON.parse(data);
+  jqueryMap.$main.empty();    // clear the content of the $main container
+  console.log(data[2].text);
 
-  console.log(data[1].text);
+  // display all the statments(questions) as paragraph from the data 
+  for (var i = 0; i < data.length; ++i){
+    jqueryMap.$main.append("<p></p>");
+    var question = data[i].text;
+    $("p:last").html( question );
+  }
 
-
-//}
+}
